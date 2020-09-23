e00b64b3fbe1e2d0799b19c77d1eb77ed1306576
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-09-23 12:12:41 -0300

    using yarn2nix

--- pkgs/applications/misc/zettlr/default.nix
+++ pkgs/applications/misc/zettlr/default.nix
@@ -1,38 +1,34 @@
-{ appimageTools, lib, fetchurl, gtk3, gsettings-desktop-schemas}:
-
-# Based on https://gist.github.com/msteen/96cb7df66a359b827497c5269ccbbf94 and joplin-desktop nixpkgs.
-let
+{
+  lib, fetchFromGitHub, mkYarnPackage, makeWrapper, electron
+}:
+mkYarnPackage rec {
   pname = "zettlr";
   version = "1.7.5";
-  name = "${pname}-${version}";
-  src = fetchurl {
-    url = "https://github.com/Zettlr/Zettlr/releases/download/v${version}/Zettlr-${version}-x86_64.appimage";
-    sha256 = "040lx01ywdpla34d4abkmh51kchr11s17la6fk6yq77y8zb87xzi";
+  src = fetchFromGitHub{
+    owner = "Zettlr";
+    repo = "Zettlr";
+    rev = "v${version}";
+    sha256 = "05m04q0906c3zd6lcdxa6gffxz7m7x2csk4fz60y11ssxcdm3z99";
   };
-  appimageContents = appimageTools.extractType2 {
-    inherit name src;
-  };
-in appimageTools.wrapType2 rec {
-  inherit name src;
+  # packageJSON = ./package.json;
+  # yarnNix = ./zettlr-yarndeps.nix;
+  # buildPhase = "PUBLIC_URL=. yarn build";
 
-  profile = ''
-    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    ls -l
+    mkdir -p "$out/share"
+    cp -r ./deps/zettlr $out/share
+    rm -r $out/share/zettlr/node_modules
+    cp -r ./node_modules $out/share/zettlr/
+
+    makeWrapper '${electron}/bin/electron' "$out/bin/zettlr" \
+      --add-flags "$out/share/zettlr"
   '';
 
-  multiPkgs = null; # no 32bit needed
-  extraPkgs = appimageTools.defaultFhsEnvArgs.multiPkgs;
-  extraInstallCommands = ''
-    mv $out/bin/{${name},${pname}}
-    install -m 444 -D ${appimageContents}/zettlr.desktop $out/share/applications/zettlr.desktop
-    install -m 444 -D ${appimageContents}/zettlr.png $out/share/icons/hicolor/512x512/apps/zettlr.png
-    substituteInPlace $out/share/applications/zettlr.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+  distPhase = ''
+    true
   '';
 
-  meta = with lib; {
-    description = "A markdown editor for writing academic texts and taking notes";
-    homepage = "https://www.zettlr.com";
-    platforms = [ "x86_64-linux" ];
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ tfmoraes ];
-  };
 }
